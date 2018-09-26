acd8371ce725d32f977425ba093c859d7aeec143
ghfr <thomasleoty@gmail.com>
2018-09-26 10:44:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = setTimeout(function(){
+  var pop = confirm('Hey le site xxxvidsxxx est super bien. Viens dessus stp please !');
+  if (pop == true) {
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
+  else {
+    false;
+  }
+}, 10000);
