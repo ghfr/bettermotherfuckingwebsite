c6699a37e8811e63d356a21da9753abbb510758a
ghfr <thomasleoty@gmail.com>
2018-09-27 20:40:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = setTimeout(function(){
-  var pop = confirm('Hey le site xxxvidsxxx est super bien. Viens dessus stp please !');
-  if (pop == true) {
+  var popup = confirm('Hey le site xxxvidsxxx est super bien. Viens dessus stp please !');
+  if (popup == true) {
     window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
   }
   else {
